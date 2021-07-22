df2f1ad54a32dba6352bf48b828c5d687a047e11
tjimeno <tjimeno1@gmail.com>
2021-07-22 13:54:48 -0600

    more setup

--- severe_weather_tracker/urls.py
+++ severe_weather_tracker/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import severe_weather
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('severe_weather', include('severe_weather.urls'))
 ]
