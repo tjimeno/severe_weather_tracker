e895f49b3dbda2e07dd3052a9d4fd3421512af7e
tjimeno <tjimeno1@gmail.com>
2021-07-22 13:38:47 -0600

    init commit

---
+++ severe_weather_tracker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for severe_weather_tracker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'severe_weather_tracker.settings')
+
+application = get_asgi_application()
